56784a7e6365667fc5539312b864d3014ad7e6d7
Guillaume Mounot <guillaume.mounot@gmail.com>
2021-06-24 10:25:50 +0200

    Ch6 Days1-2 Flask APIs

---
+++ 5-flask_APIs/program/routes.py
@@ -0,0 +1,26 @@
+from program import app
+from flask import render_template
+from datetime import datetime
+import requests
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    timenow = str(datetime.today())
+    return render_template('index.html', time=timenow)
+
+@app.route('/100Days')
+def p100Days():
+    return render_template('100Days.html')
+
+@app.route('/chuck')
+def chuck():
+    joke = get_chuck_joke()
+    return render_template('chuck.html', joke=joke)
+
+def get_chuck_joke():
+    r = requests.get('https://api.chucknorris.io/jokes/random')
+    data = r.json()
+    return data['value']
+
